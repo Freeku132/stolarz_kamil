3fd9580ca3c4e06372669ab054f372544dcd159a
Kamil Stolarz <freeku132@gmail.com>
2023-02-01 14:15:33 +0100

    Change doxy files

---
+++ html/search/all_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['_7eerrorhandle_0',['~ErrorHandle',['../class_error_handle.html#aae21d4a3ee1c0f7259519872fa1d760c',1,'ErrorHandle::~ErrorHandle()'],['../class_error_handle.html#aae21d4a3ee1c0f7259519872fa1d760c',1,'ErrorHandle::~ErrorHandle()']]],
+  ['_7esequence_1',['~Sequence',['../class_sequence.html#aee09a7d70c3ab523fed85da94ea1366f',1,'Sequence::~Sequence()'],['../class_sequence.html#aee09a7d70c3ab523fed85da94ea1366f',1,'Sequence::~Sequence()']]],
+  ['_7estatistic_2',['~Statistic',['../class_statistic.html#a273cc8bd352a4fa0c18006971c008424',1,'Statistic::~Statistic()'],['../class_statistic.html#a273cc8bd352a4fa0c18006971c008424',1,'Statistic::~Statistic()']]]
+];
---
+++ html/search/classes_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sequence_0',['Sequence',['../class_sequence.html',1,'']]],
+  ['statistic_1',['Statistic',['../class_statistic.html',1,'']]]
+];
